4b8240a374df1f97a6e42b6a92124fb3db03adc2
Wild Barreto <44129416+WildBarreto@users.noreply.github.com>
2025-09-15 15:35:22 -0300

    User entity and resource

---
+++ src/main/java/com/profwild/aulaSpring/entities/User.java
@@ -0,0 +1,76 @@
+package com.profwild.aulaSpring.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+    private Long id;
+    private String Nome;
+    private String email;
+    private String phone;
+    private String password;
+
+    public User() {
+    }
+    public User(Long id,String nome, String email, String phone, String password) {
+        this.id = id;
+        this.Nome = nome;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+}
