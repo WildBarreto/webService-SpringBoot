96e4b1577718bea9f7eb74b636eceff04d4b3db7
Wild Barreto <44129416+WildBarreto@users.noreply.github.com>
2025-11-12 22:21:14 -0300

    enums

---
+++ src/main/java/com/profwild/aulaSpring/entities/enums/OrderStatus.java
@@ -0,0 +1,29 @@
+package com.profwild.aulaSpring.entities.enums;
+
+import com.profwild.aulaSpring.entities.Order;
+
+public enum OrderStatus {
+    AGUARDANDO_PAGAMENTO(1),
+    PAGO(2),
+    ENVIADO(3),
+    ENTREGE(4),
+    CANCELADO(5);
+
+    private int code;
+    private OrderStatus(int code){
+        this.code = code;
+    }
+    public int getCode(){
+        return code;
+    }
+
+    public static OrderStatus valueOf(int code){
+        for(OrderStatus value : OrderStatus.values()){
+            if (value.getCode() ==  code){
+                return value;
+            }
+        }
+        throw  new IllegalArgumentException("Invalid Orderstatus code");
+
+    }
+}
