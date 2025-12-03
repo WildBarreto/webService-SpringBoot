f0df0da8452e8f1cc31b07d03813547037d72f31
Wild Barreto <44129416+WildBarreto@users.noreply.github.com>
2025-12-03 16:53:10 -0300

    total and subTotal

--- src/main/java/com/profwild/aulaSpring/entities/Order.java
+++ src/main/java/com/profwild/aulaSpring/entities/Order.java
@@ -91,6 +91,15 @@ public class Order implements Serializable {
     }
 
 
+    public double getTotal(){
+        double sum = 0.0;
+        for(OrderItem x : items){
+            sum += x.getSubTotal();
+        }
+        return sum;
+    }
+
+
 
     @Override
     public boolean equals(Object o) {
